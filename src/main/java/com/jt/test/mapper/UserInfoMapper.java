aa68ed4b9a31bc1020a4fac917bb4e5d71fc4ca6
jiangtan <510791331@qq.com>
2022-07-08 14:33:43 +0800

    new ：新增wx_user_info表

---
+++ src/main/java/com/jt/test/mapper/UserInfoMapper.java
@@ -0,0 +1,18 @@
+package com.jt.test.mapper;
+
+import com.jt.test.domain.UserInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Administrator
+* @description 针对表【wx_user_info】的数据库操作Mapper
+* @createDate 2022-07-08 09:55:31
+* @Entity com.jt.test.domain.UserInfo
+*/
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+
+}
+
+
+
+
