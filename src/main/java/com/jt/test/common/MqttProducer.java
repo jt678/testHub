13853923196b550c547923a2dd8ee8771d9c30ac
jiangtan <510791331@qq.com>
2022-08-04 10:16:36 +0800

    new :简单mqtt消息订阅发布支持

---
+++ src/main/java/com/jt/test/common/MqttProducer.java
@@ -0,0 +1,36 @@
+package com.jt.test.common;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MqttProducer {
+
+    private MqttClient mqttClient;
+
+    public MqttProducer(String SERVER_URI, String CLIENT_ID) {
+        try {
+            MemoryPersistence persistence = new MemoryPersistence();
+            mqttClient = new MqttClient(SERVER_URI, CLIENT_ID, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+//            System.out.println("Connecting to broker: " + SERVER_URI);
+            mqttClient.connect(connOpts);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void send(String topic, int qos, boolean retained, String payload) {
+        if (mqttClient == null) {
+            return;
+        }
+        try {
+            mqttClient.publish(topic, payload.getBytes(), qos, retained);
+        } catch (MqttException e) {
+//            System.out.println(e.getMessage());
+        }
+    }
+
+}
