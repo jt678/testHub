fdc8c057621b047d0a0864e5aae6b9173219ac0b
jiangtan <510791331@qq.com>
2022-09-13 08:54:57 +0800

    fix: 增加字段

--- src/main/java/com/jt/test/domain/entity/TestData.java
+++ src/main/java/com/jt/test/domain/entity/TestData.java
@@ -1,9 +1,11 @@
 package com.jt.test.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
+import java.util.Date;
 import lombok.Data;
 
 /**
@@ -24,6 +26,16 @@ public class TestData implements Serializable {
      */
     private String name;
 
+    /**
+     * 
+     */
+    private Date creatTime;
+
+    /**
+     * 
+     */
+    private String type;
+
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 
@@ -40,7 +52,9 @@ public class TestData implements Serializable {
         }
         TestData other = (TestData) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()));
+            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+            && (this.getCreatTime() == null ? other.getCreatTime() == null : this.getCreatTime().equals(other.getCreatTime()))
+            && (this.getType() == null ? other.getType() == null : this.getType().equals(other.getType()));
     }
 
     @Override
@@ -49,6 +63,8 @@ public class TestData implements Serializable {
         int result = 1;
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getCreatTime() == null) ? 0 : getCreatTime().hashCode());
+        result = prime * result + ((getType() == null) ? 0 : getType().hashCode());
         return result;
     }
 
@@ -60,6 +76,8 @@ public class TestData implements Serializable {
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
         sb.append(", name=").append(name);
+        sb.append(", creatTime=").append(creatTime);
+        sb.append(", type=").append(type);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
