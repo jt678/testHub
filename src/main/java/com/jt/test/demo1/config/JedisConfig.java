0bded7022857d1c1120ac925ea58a1c6a58ce0ae
jiangtan <510791331@qq.com>
2022-12-07 10:56:48 +0800

    feat(配置类): Jedis配置类名修改，用来区分redisTemplate基础配置

--- src/main/java/com/jt/test/demo1/config/JedisConfig.java
+++ src/main/java/com/jt/test/demo1/config/JedisConfig.java
@@ -8,17 +8,19 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 /**
  * RedisConfig
- *
+ *  Jedis配置
  * @author jt
  * @date 2022/5/5
  **/
 @Configuration
-public class RedisConfig {
+public class JedisConfig {
     @Bean
     public RedisConnectionFactory redisCF(){
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
+        configuration.setHostName("127.0.0.1");
         configuration.setPort(6379);
+        //用db1做jedis库
+        configuration.setDatabase(1);
         return new JedisConnectionFactory(configuration);
     }
 }
