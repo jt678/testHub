d159ef2d3658759c8ced24914124338e0118082d
jiangtan <510791331@qq.com>
2022-12-09 10:02:50 +0800

    feat(dict): 设置主键

--- src/main/java/com/jt/test/demo1/domain/entity/Dict.java
+++ src/main/java/com/jt/test/demo1/domain/entity/Dict.java
@@ -1,6 +1,8 @@
 package com.jt.test.demo1.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
@@ -16,6 +18,7 @@ public class Dict implements Serializable {
     /**
      * 
      */
+    @TableId(type = IdType.AUTO)
     private String dictId;
 
     /**
