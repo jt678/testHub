85994e6d947cdb10d450946edde56413b74532de
jiangtan <510791331@qq.com>
2022-12-13 16:08:04 +0800

    feat(redis配置类): 增加注释，配置修改

--- src/main/java/com/jt/test/demo1/config/RedisConfig.java
+++ src/main/java/com/jt/test/demo1/config/RedisConfig.java
@@ -1,5 +1,6 @@
 //package com.jt.test.demo1.config;
 //
+//import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,19 +9,25 @@
 //
 ///**
 // * RedisConfig
-// *  redis连接配置------这里如果配置了，yml里面再配置，优先读取这里的，yml失效
+// * redis连接配置------这里如果配置了，yml里面再配置，优先读取这里的，yml失效
+// * 原因:yml也是通过引入的spring-data-redis包下通过自动装配来实现的，如果再设置一次会覆盖之前的设置
 // * @author jt
 // * @date 2022/5/5
 // **/
+//// configuration,service,controller,component,repository这些注解spring都默认了单例
 //@Configuration
 //public class RedisConfig {
+//    @Value("spring.redis.database")
+//    private int database;
+//    @Value("spring.redis.host")
+//    private String host;
+//
 //    @Bean
 //    public RedisConnectionFactory redisCF(){
 //        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-//        configuration.setHostName("127.0.0.1");
+//        configuration.setHostName(host);
 //        configuration.setPort(6379);
-//        //用db1做jedis库
-//        configuration.setDatabase(1);
+//        configuration.setDatabase(database);
 //        return new JedisConnectionFactory(configuration);
 //    }
 //}
